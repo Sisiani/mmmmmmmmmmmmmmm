4b51fc9a7a7e5e07f75847c4c410527214e323d9
Alireza <122238514+Sisiani@users.noreply.github.com>
2025-10-01 00:03:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from telegram.ext import (
 
 # ============== CONFIG ==============
 TOKEN = os.environ.get("TOKEN", "8311865694:AAHrQDLSJcFKOztBj8X2PtMafk7U7AML0Uo")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "6277910360"))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "7374971382"))
 GROUP_ID = int(os.environ.get("GROUP_ID", "-1003086390705"))
 CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/NEURANAcademy")
 
